45e23f2768719f83f9b11c68ce71e5dc6d9a7268
Moros Smith <moros1138@gmail.com>
2025-05-07 07:42:02 -0400

    remove console show & hide

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,6 @@
 
 int main()
 {
-#if defined(OLC_PLATFORM_WINAPI)
-	#ifdef _DEBUG
-		ShowWindow(GetConsoleWindow(), SW_SHOW);
-	#else
-		ShowWindow(GetConsoleWindow(), SW_HIDE);
-	#endif
-#endif	
-	
 	if (game->Construct(480, 320, 2, 2, false, true))
 		game->Start();
 	delete game;
